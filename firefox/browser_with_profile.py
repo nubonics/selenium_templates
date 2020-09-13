f0caed36fe01e45d06bb3018603b36b5b0f4c052
nubonics <leagueofzion@hotmail.com>
2020-09-12 17:24:40 -0700

    updated firefox browser_with_profile.py and added a requirements.txt file

--- firefox/browser_with_profile.py
+++ firefox/browser_with_profile.py
@@ -1,17 +1,23 @@
 import getpass
+import platform
 
+from glob import glob
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver import DesiredCapabilities
 
 username = getpass.getuser()
-gecko_driver_path = '/home/nubonix/webdrivers/firefox/geckodriver'
+if platform.system() == 'linux':
+    gecko_driver_path = f'/home/{username}/webdrivers/firefox/geckodriver'
+    profile_path = "/home/{username}/.mozilla/firefox/or2171cc.recaptcha"
+if platform.system() == 'windows':
+    gecko_driver_path = f'C:/Users/{username}/webdrivers/geckodriver/geckodriver.exe'
+    profile_path = glob("%APPDATA%/Mozilla/Firefox/Profiles/*.recaptcha")[0]
+else:
+    raise Exception('Could not detect operating system')
+
 options = Options()
 options.add_argument("-profile")
-options.add_argument(f"/home/{username}/.mozilla/firefox/or2171cc.recaptcha")
-firefox_capabilities = DesiredCapabilities.FIREFOX
-firefox_capabilities['marionette'] = True
-# driver = webdriver.Firefox(executable_path=gecko_driver_path, capabilities=firefox_capabilities, options=options)
+options.add_argument(profile_path)
 driver = webdriver.Firefox(executable_path=gecko_driver_path, options=options)
 
 driver.get('about:profiles')
